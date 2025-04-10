c1130e8d38ca8b8e210cd1ea3dd38fb2e8435daf
samug0 <galupposamuele@gmail.com>
2025-04-10 19:51:38 +0200

    updated config

--- pizza_hub/settings.py
+++ pizza_hub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +85,6 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT", '5432'),
     },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
